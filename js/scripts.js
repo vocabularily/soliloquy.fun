3666b6cfb2190737c8af88dd84369f084a9934f3
Slawomir Onyszko <slawomir.onyszko@gmail.com>
2025-01-12 12:14:15 +0100

    Fix images and missing blog link

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
-* Copyright 2013-2024 Start Bootstrap
+* Copyright 2013-2025 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
 */
 //
